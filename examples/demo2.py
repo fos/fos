bfa68bb3273793ac0fe6b7dfdd42eca0a85ec0af
Stephan Gerhard <git@unidesign.ch>
2011-07-22 21:03:34 +0200

    Started with Shader demo actor

---
+++ examples/demo2.py
@@ -0,0 +1,10 @@
+# Need to start with ipython --gui qt
+# Shows a Shader
+
+from fospyside import *
+w = Window()
+w.show()
+
+#w.add_actor( ShaderActor() )
+
+
--- fospyside/actor.py
+++ fospyside/actor.py
@@ -1,4 +1,66 @@
 from pyglet.gl import *
+from PySide.QtOpenGL import QGLShader, QGLShaderProgram
+
+from PySide.QtGui import QMatrix4x4
+
+from vsml import vsml
+
+vert = """#version 130
+            in vec3 aPosition;
+            in vec4 aColor; // This is the per-vertex color
+            // matrices
+            uniform mat4 projMatrix;
+            uniform mat4 modelviewMatrix;
+            out vec4 vColor;   // This is the output to the geometry shader
+            void main()
+            {
+                    vColor = vec4(aColor.x , aColor.y , aColor.z, aColor.w); 
+                    gl_Position = projMatrix * modelviewMatrix * vec4(aPosition.x , aPosition.y, aPosition.z, 1.0);
+            }"""
+frag = """#version 130
+        in vec4 vColor; void main(){    gl_FragColor = vec4(vColor.x, vColor.y, vColor.z,  vColor.w); }"""
+
+
+class ShaderActor(object):
+    def __init__(self):
+        """ Only create this actor of a valid OpenGL context exists
+        """
+
+        shader = QGLShader(QGLShader.Fragment)
+        shader.compileSourceCode(frag)
+
+        self.program = QGLShaderProgram()
+        self.program.addShader(shader)
+
+        shader = QGLShader(QGLShader.Vertex)
+        shader.compileSourceCode(vert)
+
+        self.program.addShader(shader)
+
+        self.program.link()
+        self.program.bind()
+
+        self.aPosition = self.program.attributeLocation("aPosition")
+        self.aColor = self.program.attributeLocation("aColor")
+        self.projMatrix = self.program.uniformLocation("projMatrix")
+        self.modelviewMatrix = self.program.uniformLocation("modelviewMatrix")
+
+        # TODO: retrieve tuple array row-major order QMatrix4x4(vsml.get_projection())
+
+
+    def draw(self):
+
+        # http://www.pyside.org/docs/pyside/PySide/QtOpenGL/QGLShaderProgram.html
+        self.program.enableAttributeArray( self.aPosition )
+        
+        #program.setAttributeArray(vertexLocation, triangleVertices, 3)
+        #program.setUniformValue(matrixLocation, pmvMatrix)
+        #program.setUniformValue(colorLocation, color)
+        #glDrawArrays(GL_TRIANGLES, 0, 3)
+        #self.program.disableAttributeArray(vertexLocation)
+
+
+    
 
 class Axes(object):
 
