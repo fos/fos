b8707df7391e71ab30b962b4ec41fb55750eebb2
Stephan Gerhard <git@unidesign.ch>
2011-07-29 17:32:54 +0200

    Start with Microcircuit actor

---
+++ fos/actor/microcircuit.py
@@ -0,0 +1,26 @@
+import numpy as np
+from pyglet.gl import *
+from .base import *
+
+class Microcircuit(Actor):
+
+    def __init__(self, name ):
+        """ A microcircuitry actor consisting of skeletons, connectors
+        and pre and post connectivity
+        """
+
+        # for skeletons
+        # - vertices
+        # - connectivity
+        # - skeleton_properties (labels, ...)
+
+        # for connectors
+        # - vertices
+        # - connector_properties (labels, ...)
+        
+        # skeleton (tree)node with connectors
+        # - connectivity
+        # - t_c_properties (pre, post)
+
+        # need to create basic primitives
+        # sphere, tube
