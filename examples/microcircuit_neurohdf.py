ecba8ba2ad5887e66f3a42907254d1614d784a4d
Stephan Gerhard <git@unidesign.ch>
2011-11-22 17:46:28 +0100

    Make a function

--- examples/microcircuit_neurohdf.py
+++ examples/microcircuit_neurohdf.py
@@ -13,12 +13,13 @@ connectivity_type = f['Microcircuit']['connectivity']['type'].value
 print connectivity_type
 vertices_location = ((vertices_location - np.mean(vertices_location, axis=0))).astype(np.float32)
 
-
-map_vertices_id2index = dict(zip(vertices_id,range(len(vertices_id))))
-connectivity_indices = np.zeros( connectivity_id.shape, dtype=np.uint32 )
-for i,c in enumerate(connectivity_id):
-    connectivity_indices[i,0]=map_vertices_id2index[connectivity_id[i,0]]
-    connectivity_indices[i,1]=map_vertices_id2index[connectivity_id[i,1]]
+def map_vertices_id2index(vertices_id):
+    map_vertices_id2index = dict(zip(vertices_id,range(len(vertices_id))))
+    connectivity_indices = np.zeros( connectivity_id.shape, dtype=np.uint32 )
+    for i,c in enumerate(connectivity_id):
+        connectivity_indices[i,0]=map_vertices_id2index[connectivity_id[i,0]]
+        connectivity_indices[i,1]=map_vertices_id2index[connectivity_id[i,1]]
+    return connectivity_indices
 
 conn_color_map = {
     1 : np.array([[1.0, 1.0, 0.0, 1.0]]),
@@ -26,7 +27,7 @@ conn_color_map = {
     3 : np.array([[1.0, 0.0, 0.0, 1.0]])
 }
 
-w = Window( )
+w = Window( dynamic=True )
 
 mytransform = Transform3D(np.eye(4))
 mytransform.set_scale(0.1, 0.1, 0.1)
@@ -34,9 +35,9 @@ mytransform.set_scale(0.1, 0.1, 0.1)
 region = Region( regionname = "Main", transform = mytransform)
 
 act = Microcircuit(
-    name = "Testcircuit",
-    vertices_location = vertices_location,
-    connectivity = connectivity_indices,
+    name="Testcircuit",
+    vertices_location=vertices_location,
+    connectivity=map_vertices_id2index(vertices_id),
     connectivity_ids=connectivity_skeletonid,
     connectivity_label=connectivity_type,
     connectivity_label_metadata=[
@@ -44,7 +45,7 @@ act = Microcircuit(
                         { "name" : "presynaptic", "value" : "2" },
                         { "name" : "postsynaptic", "value" : "3" }
                     ],
-    connectivity_colormap = conn_color_map
+    connectivity_colormap=conn_color_map
 )
 
 region.add_actor( act )
@@ -55,5 +56,7 @@ values = np.ones( (len(vertices_location)) ) * 100
 region.add_actor( Scatter( "MySphere", vertices_location[:,0], vertices_location[:,1], vertices_location[:,2], values, iterations = 2 ) )
 
 w.add_region ( region )
+
+act.deselect_all()
 
 w.refocus_camera()
