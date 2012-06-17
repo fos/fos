851c6f9d2712772d58716a5b3627beae69e073f3
Stephan Gerhard <git@unidesign.ch>
2012-06-17 18:58:05 +0200

    Triangulation example. Useful for 2D maps

--- fos/util.py
+++ fos/util.py
@@ -5,3 +5,9 @@ def reindex_connectivity( vert, conn):
     conn = np.array( range(len(vert)), dtype = np.uint32 )
     conn = conn.reshape( (len(conn)/2, 2) )
     return vert, conn
+
+import matplotlib.delaunay as triang
+
+def find_triangulation2d(x,y):
+    cens,edg,tri,neig = triang.delaunay(x,y)
+    return tri
