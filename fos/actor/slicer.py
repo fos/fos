05a9e1b5cb1bdcfc36d379a938219e5a2e3c8287
Eleftherios Garyfallidis <garyfallidis@gmail.com>
2012-10-15 17:57:11 +0200

    NF Slicer agrees with FSL convention

--- fos/actor/slicer.py
+++ fos/actor/slicer.py
@@ -6,6 +6,20 @@ from pyglet.gl import *
 
 class Slicer(Actor):
     def __init__(self, name, data, affine=None):
+        """ Volume Slicer
+
+        Parameters
+        ----------
+        name : str
+        data : array, (X, Y, Z) or (X, Y, Z, 3) or (X, Y, Z, 4)
+        affine : not implemented (default None)
+
+        Notes
+        ------
+        Coordinate Systems
+        http://www.grahamwideman.com/gw/brain/orientation/orientterms.htm
+        http://www.slicer.org/slicerWiki/index.php/Coordinate_systems
+        """
 
         self.name = name
         self.data = data
@@ -14,42 +28,63 @@ class Slicer(Actor):
                                 type=GL_RGBA, interp=GL_LINEAR)
         self.vertices = self.tex.vertices
         self.visible = True
-        i, j, k = self.data.shape[:3]
-        self.texcoords_i, self.vertcoords_i = self.tex.slice_i(i/2) 
-        self.texcoords_j, self.vertcoords_j = self.tex.slice_j(j/2) 
-        self.texcoords_k, self.vertcoords_k = self.tex.slice_k(k/2) 
+        self.i, self.j, self.k = self.data.shape[:3]
+        self.texcoords_i, self.vertcoords_i = self.tex.slice_i(self.i/2) 
+        self.texcoords_j, self.vertcoords_j = self.tex.slice_j(self.j/2) 
+        self.texcoords_k, self.vertcoords_k = self.tex.slice_k(self.k/2) 
         #tex.update_quad(texcoords, vertcoords)
+
+        self.show_i = True
+        self.show_j = True
+        self.show_k = True
+
+    def slice_i(self, i):
+        self.i = i
+        self.texcoords_i, self.vertcoords_i = self.tex.slice_i(i) 
+
+    def slice_j(self, j):
+        self.j = j
+        self.texcoords_j, self.vertcoords_j = self.tex.slice_j(j) 
+
+    def slice_k(self, k):
+        self.k = k
+        self.texcoords_k, self.vertcoords_k = self.tex.slice_k(k)        
 
     def draw(self):
         
-        #i Slice
-        glPushMatrix()
-        glRotatef(90, 0., 1., 0)
-        glRotatef(90, 0., 0., 1.)
-        self.tex.update_quad(self.texcoords_i, self.vertcoords_i)
-        self.tex.set_state()
-        self.tex.draw()
-        self.tex.unset_state()
-        glPopMatrix()
+        if self.show_i: 
+            #i Slice
+            glPushMatrix()
+            glRotatef(90, 0., 1., 0)
+            glRotatef(90, 0., 0., 1.)
+            self.tex.update_quad(self.texcoords_i, self.vertcoords_i)
+            self.tex.set_state()
+            self.tex.draw()
+            self.tex.unset_state()
+            glPopMatrix()
         
         #j Slice
-        glPushMatrix()
-        glRotatef(90, 0., 0., 1.)
-        self.tex.update_quad(self.texcoords_j, self.vertcoords_j)
-        self.tex.set_state()
-        self.tex.draw()
-        self.tex.unset_state()
-        glPopMatrix()
+        if self.show_j:
+            glPushMatrix()
+            glRotatef(180, 0., 1., 0) # added for fsl convention
+            glRotatef(90, 0., 0., 1.)
+            self.tex.update_quad(self.texcoords_j, self.vertcoords_j)
+            self.tex.set_state()
+            self.tex.draw()
+            self.tex.unset_state()
+            glPopMatrix()
 
-        #k Slice
-        glPushMatrix()
-        glRotatef(90, 1., 0, 0.)
-        glRotatef(90, 0., 0., 1)
-        self.tex.update_quad(self.texcoords_k, self.vertcoords_k)
-        self.tex.set_state()
-        self.tex.draw()
-        self.tex.unset_state()
-        glPopMatrix()
+        if self.show_k:
+            #k Slice
+            glPushMatrix()
+            glRotatef(90, 1., 0, 0.)
+            glRotatef(90, 0., 0., 1)
+            glRotatef(180, 1., 0., 0.) # added for fsl
+            self.tex.update_quad(self.texcoords_k, self.vertcoords_k)
+            self.tex.set_state()
+            self.tex.draw()
+            self.tex.unset_state()
+            glPopMatrix()
         
 
 if __name__ == '__main__':
@@ -63,14 +98,14 @@ if __name__ == '__main__':
     data = img.get_data()
     data = np.interp(data, [data.min(), data.max()], [0, 255])
 
-    window = Window(caption="BuzzTex with Free on Shades", 
+    window = Window(caption="Interactive Slicer with Free on Shades", 
                         bgcolor=(0.4, 0.4, 0.9))
     region = Region(activate_aabb=False)
 
     slicer = Slicer('BuzzTex', data)
     region.add_actor(slicer)
     window.add_region(region)
-
+    window.refocus_camera()
     
     #ax = Axes(name="3 axes", scale=200, linewidth=10.0)
     #region.add_actor(ax)
